c4d45f142ab77e0cde6ca1720aa77a64dc39cab1
Gabriel Salvador Lopes Reis <reissalvador2004@gmail.com>
2022-07-15 11:18:56 -0300

    Estrutura base

---
+++ JS/modal.js
@@ -0,0 +1,17 @@
+function alterModal(type, alter) {
+    const modal = document.getElementById("modal");
+    if (modal.classList.contains(alter));
+        modal.classList.remove('ini')
+        modal.classList.remove(alter);
+        modal.classList.add(type);
+        
+};
+
+function startModal(show){
+    const modal = document.getElementById("modal");
+    modal.classList.add(show);
+    modal.addEventListener('click', function(exit) {
+        if (exit.target.id == 'modal')
+            modal.classList.remove(show)
+    })
+};
