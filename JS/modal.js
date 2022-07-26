e5ab3d570e4182fa7ad6c267422b54c991ec2e46
Gabriel Salvador Lopes Reis <reissalvador2004@gmail.com>
2022-07-25 23:01:48 -0300

    refazendo a landing page

--- JS/modal.js
+++ JS/modal.js
@@ -21,7 +21,7 @@ function pullList(gatilho, lista, icone){
     const icon = document.getElementById(icone)
 
     if (trigger.classList.contains('hidden')) {
-        list.style.animation = "hiddenList 0.5s";
+        list.style.animation = "hiddenList 0.6s";
         list.style.maxHeight = "1px"
         trigger.classList.remove("hidden")
         icon.style.animation = "downAngle 0.5s"
@@ -29,9 +29,22 @@ function pullList(gatilho, lista, icone){
     } else {
         list.style.animation = "useList 0.5s";
         list.style.height = "auto"
-        list.style.maxHeight = "150px"
+        list.style.maxHeight = "200px"
         trigger.classList.add("hidden")
         icon.style.animation = "upAngle 0.5s"
         icon.style.transform = "rotate(180deg)"
     }
-};
+};  
+
+/*
+function pullFunc(itemValue, item, card) {
+    var value = document.getElementById(itemValue).value;
+    var funcionario = document.getElementById(card)
+    var item = document.getElementById(item)
+    if (funcionario.classList.contains(value)) {
+        console.log("oi")
+    } else {
+        console.log("mersdasd")
+    }
+}
+*/
